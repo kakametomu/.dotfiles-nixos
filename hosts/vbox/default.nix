42510feb96700e7bbc4df1007542ab0662676d7e
kaka <kaka@example.com>
2026-04-12 17:11:41 +0900

    KDEテスト用

--- hosts/vbox/default.nix
+++ hosts/vbox/default.nix
@@ -97,9 +97,13 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
+  # Enable the KDE Plasma 6 Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
   # Enable the GNOME Desktop Environment.
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
+  # services.displayManager.gdm.enable = true;
+  # services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
